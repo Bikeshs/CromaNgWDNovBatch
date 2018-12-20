1fd27d9c1a8f0661984090a649d0fec0cb63d64e
Bikesh <bikesh1988@gmail.com>
2018-12-20 10:14:16 +0530

    error fixes

--- src/app/routeexample/routerappmodule.ts
+++ src/app/routeexample/routerappmodule.ts
@@ -2,8 +2,12 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { StartComponent } from "./start.component";
+import { AComponent } from "./a.component";
+import { CComponent } from "./c.component";
+import { LoginComponent } from "./login.component";
+import { BComponent } from "./b.component";
 @NgModule({
-    declarations: [],
+    declarations: [StartComponent, AComponent, BComponent, CComponent, LoginComponent],
     imports: [BrowserModule, FormsModule],
     providers: [],
     bootstrap: [StartComponent]
